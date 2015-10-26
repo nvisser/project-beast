db84ade446922ea3e1e6171209190abf39ab81fa
Niek <niek.visser@student.hu.nl>
2015-10-26 13:10:05 +0100

    Tekst vervangen

--- Tkinter.py
+++ Tkinter.py
@@ -2,20 +2,25 @@ __author__ = 'Brumes'
 
 from tkinter import *
 
-global window
+global window, infotekst
 window = Tk()
 
 
 def laatschermzien(api):
     label = Label(window, text='Van welk station wilt u de vertrektijden weten?')
     label.pack()
-    data = api.verkrijg_utrecht()
-    button = Button(window, text='Utrecht', command=lambda: zettekstneer(data))
-    button.pack()
+    utrecht = api.zoek('Utrecht')
+    groningen = api.zoek('Groningen')
+    button1 = Button(window, text='Utrecht', command=lambda: zettekstneer(utrecht))
+    button1.pack()
+    button2 = Button(window, text='Groningen', command=lambda: zettekstneer(groningen))
+    button2.pack()
 
     window.mainloop()
 
 
 def zettekstneer(data):
-    label = Label(window, text=data)
+    infotekst = data
+    # TODO: label tekst vervangen
+    label = Label(window, text=infotekst)
     label.pack()
