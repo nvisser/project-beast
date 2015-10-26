8202b98d43a6573248c93e20d70741f1308b5720
Niek <niek.visser@student.hu.nl>
2015-10-26 12:55:44 +0100

    Lambda die shit

--- Tkinter.py
+++ Tkinter.py
@@ -7,7 +7,7 @@ def laatschermzien(api):
     window = Tk()
     label = Label(window, text='Van welk station wilt u de vertrektijden weten?')
     label.pack()
-    button = Button(window, text='Utrecht', command=api.verkrijg_utrecht())
+    button = Button(window, text='Utrecht', command=lambda: api.verkrijg_utrecht())
     button.pack()
 
     window.mainloop()
