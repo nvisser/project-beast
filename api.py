d5f71790bfc4be13a92c68500569523938959526
Niek <niek.visser@student.hu.nl>
2015-10-26 12:28:13 +0100

    API tweaks

--- api.py
+++ api.py
@@ -2,19 +2,25 @@
 import xmltodict
 import requests
 
+waar = input('waar vandaan yo? ')
+
 api_url = 'http://webservices.ns.nl/ns-api-avt?station=%s'
 api_user = 'niek.visser@student.hu.nl'
 api_pass = 'nagqEQlEI_xbnOtbilwoAyT_lCSNd5O0XnBywc4Gv2x0Y_O9QHuH8g'
 
 headers = {
     'Authorization': 'Basic bmllay52aXNzZXJAc3R1ZGVudC5odS5ubDpuYWdxRVFsRUlfeGJuT3RiaWx3b0F5VF9sQ1NOZDVPMFhuQnl3YzRHdjJ4MFlfTzlRSHVIOGc='}
-data = requests.get(api_url % 'Utrecht', headers=headers)
+data = requests.get(api_url % waar, headers=headers)
 
 data_text = data.text
 ez_access = xmltodict.parse(data_text)
+if 'error' in ez_access:
+    print(ez_access['error']['message'])
+    exit(1)
 for train in ez_access['ActueleVertrekTijden']['VertrekkendeTrein']:
-    print("Trein {} van Utrecht naar {} vertrekt om {}".format(
+    print("Trein {} van {} naar {} vertrekt om {}".format(
         train['RitNummer'],
+        waar,
         train['EindBestemming'],
         train['VertrekTijd']
     ))
