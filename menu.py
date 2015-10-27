fbbd94104afdf3d06de1cb6fd4e77c4dcafc7aba
Niek <niek.visser@student.hu.nl>
2015-10-27 14:04:55 +0100

    Formatting and window size

--- menu.py
+++ menu.py
@@ -3,25 +3,27 @@ __author__ = 'Brumes'
 from tkinter import *
 
 window = Tk()
+window.geometry('{}x{}'.format(1280, 720))
+
 window.configure(background='yellow')
-mmbutton = Button (window, text = "Placeholder")
-mmbutton.place( x=200, y = 200)
+mmbutton = Button(window, text="Placeholder")
+mmbutton.place(x=200, y=200)
 mmbutton.config(width=20, height=10)
 
-mmbutton = Button (window, text = "Placeholder")
-mmbutton.place( x=400, y = 200)
+mmbutton = Button(window, text="Placeholder")
+mmbutton.place(x=400, y=200)
 mmbutton.config(width=20, height=10)
 
-mmbutton = Button (window, text = "Placeholder")
-mmbutton.place( x=600, y = 200)
+mmbutton = Button(window, text="Placeholder")
+mmbutton.place(x=600, y=200)
 mmbutton.config(width=20, height=10)
 
-mmbutton = Button (window, text = "Placeholder")
-mmbutton.place( x=800, y = 200)
+mmbutton = Button(window, text="Placeholder")
+mmbutton.place(x=800, y=200)
 mmbutton.config(width=20, height=10)
 
-mmbutton = Button (window, text = "Placeholder")
-mmbutton.place( x=1000, y = 200)
+mmbutton = Button(window, text="Placeholder")
+mmbutton.place(x=1000, y=200)
 mmbutton.config(width=20, height=10)
 
 window.mainloop()
