ee7bdf9fda8e524b75aa409c95d8c3c5665f8c15
BrumesWolf <bram.stoker@student.hu.nl>
2015-10-27 14:28:43 +0100

    Start menu kleuren en opmaak fixes

--- menu.py
+++ menu.py
@@ -1,27 +1,35 @@
 __author__ = 'Brumes'
 
 from tkinter import *
+from tkinter.messagebox import showinfo
+def popup():
+    showinfo(title='Popup', message='This button is useless')
 
 window = Tk()
-window.configure(background='yellow')
-mmbutton = Button (window, text = "Placeholder")
-mmbutton.place( x=200, y = 200)
-mmbutton.config(width=20, height=10)
+window.configure(background='#FFE917')
 
-mmbutton = Button (window, text = "Placeholder")
-mmbutton.place( x=400, y = 200)
-mmbutton.config(width=20, height=10)
+toptext=Label(window, text='Welkom bij de NS \n Selecteer een optie')
+toptext.place(x=450, y=150)
+toptext.config(background='#FFE917', foreground='#006', font='Arial 32 bold')
 
-mmbutton = Button (window, text = "Placeholder")
-mmbutton.place( x=600, y = 200)
-mmbutton.config(width=20, height=10)
+mmbutton = Button (window, text = "Kopen \n los kaartje", command=lambda: popup())
+mmbutton.place( x=70, y = 350)
+mmbutton.config(width=20, height=10, background='#003082', foreground='white', font='Arial 14 bold')
 
-mmbutton = Button (window, text = "Placeholder")
-mmbutton.place( x=800, y = 200)
-mmbutton.config(width=20, height=10)
 
-mmbutton = Button (window, text = "Placeholder")
-mmbutton.place( x=1000, y = 200)
-mmbutton.config(width=20, height=10)
+mmbutton = Button (window, text = "Kopen \n OV-chipkaart", command=lambda: popup())
+mmbutton.place( x=370, y = 350)
+mmbutton.config(width=20, height=10, background='#003082', foreground='white', font='Arial 14 bold')
+command = popup
+
+mmbutton = Button (window, text = "Ik wil naar \n het buitenland", command=lambda: popup())
+mmbutton.place( x=670, y = 350)
+mmbutton.config(width=20, height=10, background='#003082', foreground='white', font='Arial 14 bold')
+command = popup
+
+mmbutton = Button (window, text = "Actuale \n vertrektijden", command=lambda: popup())
+mmbutton.place( x=970, y = 350)
+mmbutton.config(width=20, height=10, background='#003082', foreground='white', font='Arial 14 bold')
+command = popup
 
 window.mainloop()
