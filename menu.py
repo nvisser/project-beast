4f89e75c348a1ac85b3e97efb67d0cbe3a2d09c6
MilanvandeWerken <milan.vandewerken@student.hu.nl>
2015-10-27 16:05:19 +0100

    Bugfix van gele vierkant :)

--- menu.py
+++ menu.py
@@ -1,6 +1,5 @@
 from tkinter import *
 from tkinter.messagebox import showinfo
-import Tkinter
 import api
 
 
@@ -8,6 +7,7 @@ def popup():
     showinfo(title='Popup', message='This button is useless')
 
 def submenu():
+    import Tkinter
     Tkinter.laatschermzien(api)
 
 
@@ -18,25 +18,25 @@ window.geometry('{}x{}'.format(1920, 1080))
 
 
 toptext = Label(window, text='Welkom bij de NS \n Selecteer een optie')
-toptext.place(x=450, y=150)
+toptext.place(x=550, y=150)
 toptext.config(background='#FFE917', foreground='#006', font='Arial 32 bold')
 
 mmbutton = Button(window, text="Kopen \n los kaartje", command=lambda: popup())
-mmbutton.place(x=70, y=350)
+mmbutton.place(x=230, y=350)
 mmbutton.config(width=20, height=10, background='#003082', foreground='white', font='Arial 14 bold')
 
 mmbutton = Button(window, text="Kopen \n OV-chipkaart", command=lambda: popup())
-mmbutton.place(x=370, y=350)
+mmbutton.place(x=510, y=350)
 mmbutton.config(width=20, height=10, background='#003082', foreground='white', font='Arial 14 bold')
 
 
 mmbutton = Button(window, text="Ik wil naar \n het buitenland", command=lambda: popup())
-mmbutton.place(x=670, y=350)
+mmbutton.place(x=790, y=350)
 mmbutton.config(width=20, height=10, background='#003082', foreground='white', font='Arial 14 bold')
 
 
-mmbutton = Button(window, text="Actuale \n vertrektijden", command=lambda: submenu())
-mmbutton.place(x=970, y=350)
+mmbutton = Button(window, text="Actuele \n vertrektijden", command=lambda: submenu())
+mmbutton.place(x=1070, y=350)
 mmbutton.config(width=20, height=10, background='#003082', foreground='white', font='Arial 14 bold')
 
 
